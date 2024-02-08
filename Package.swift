d460b7418342fe83d712d97c604692584e97ccf4
Jakub Kalamarz <jakub.k@survicate.com>
2024-02-08 10:12:18 +0100

    Implementation of UXCam integration

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -11,11 +11,20 @@ let package = Package(
             name: "survicate-uxcam-integration",
             targets: ["survicate-uxcam-integration"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/Survicate/survicate-ios-sdk", from: "4.1.0"),
+        .package(url: "https://github.com/uxcam/ios-sdk", branch: "main"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "survicate-uxcam-integration"),
+            name: "survicate-uxcam-integration",
+            dependencies: [
+                .product(name: "UXCam", package: "ios-sdk"),
+                .product(name: "Survicate", package: "survicate-ios-sdk"),
+            ]
+        ),
         .testTarget(
             name: "survicate-uxcam-integrationTests",
             dependencies: ["survicate-uxcam-integration"]),
